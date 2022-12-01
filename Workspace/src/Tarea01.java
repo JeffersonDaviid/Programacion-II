aa5deae28688cd745f498fedce4ce131884a734b
Jefferson David <jeffersonchile2001@gmail.com>
2022-12-01 00:19:18 -0500

    tercera revision

--- Workspace/src/Tarea01.java
+++ Workspace/src/Tarea01.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import javax.swing.JOptionPane;
 
 public class Tarea01 {
     public static void presentarObjeto(String objetos[]) {
@@ -10,21 +9,25 @@ public class Tarea01 {
 
     public static boolean verificarElemento(String elemento, String grupo[]) {
         for (int elem = 0; elem < grupo.length; elem++)
-            if (grupo[elem] != null)
-                if (grupo[elem].equals(elemento) && !grupo[elem].equals("n"))
-                    return true;
+            if (grupo[elem] != null && grupo[elem].equals(elemento) && !grupo[elem].equals("n"))
+                return true;
         return false;
     }
 
-    public static void moverElemento(String valor, String grupoOrigen[], String grupoLlegada[]) {
-        for (int elemento = 0; elemento < grupoOrigen.length; elemento++)
-            if (grupoOrigen[elemento] == null || grupoOrigen[elemento].equals(valor))
-                grupoOrigen[elemento] = "n";
+    public static void moverElemento(String valor, String grupoSalida[], String grupoLlegada[]) {
+        for (int elemento = 0; elemento < grupoSalida.length; elemento++)
+            if (grupoSalida[elemento] == null || grupoSalida[elemento].equals(valor))
+                grupoSalida[elemento] = "n";
         for (int elemento = 0; elemento < grupoLlegada.length; elemento++)
             if (grupoLlegada[elemento] == null || grupoLlegada[elemento].equals("n")) {
                 grupoLlegada[elemento] = valor;
                 break;
             }
+        if ((verificarElemento("L", grupoSalida) && verificarElemento("C", grupoSalida))
+                || verificarElemento("C", grupoSalida) && verificarElemento("U", grupoSalida)) {
+            System.out.println("You lost :(");
+            System.exit(0);
+        }
     }
 
     public static void main(String[] args) throws Exception {
@@ -42,34 +45,24 @@ public class Tarea01 {
             System.out.println("\nElementos a la derecha del rio");
             presentarObjeto(derecha);
             if (p_izquierda == true) {
-                System.out.println("\nPorfavor ingrese la inicial del elemento a mover a la derecha: ");
+                System.out.println("\nPorfavor ingrese la inicial del elemento a mover a la derecha ");
                 eleccion = teclado.next().toUpperCase();
-                if ((eleccion.equals("L") || eleccion.equals("C") || eleccion.equals("U"))
-                        && verificarElemento(eleccion, izquierda)) {
+                if (verificarElemento(eleccion, izquierda)
+                        && (eleccion.equals("L") || eleccion.equals("C") || eleccion.equals("U"))) {
                     moverElemento(eleccion, izquierda, derecha);
                     p_izquierda = false;
-                    if ((verificarElemento("L", izquierda) && verificarElemento("C", izquierda))
-                            || verificarElemento("C", izquierda) && verificarElemento("U", izquierda)) {
-                        JOptionPane.showMessageDialog(null, "You lost :(");
-                        return;
-                    }
                 }
             } else {
                 System.out.println(
-                        "\nEsta por volver a la izquierda, ¿Desea volver con un elemento? \n(Si) ingrese inicial del elemento\n(No) presione cualquier tecla: ");
+                        "\nEsta por volver a la izquierda, ¿Desea volver con un elemento? \n(Si) ingrese inicial del elemento\n(No) presione cualquier tecla ");
                 eleccion = teclado.next().toUpperCase();
-                if ((eleccion.equals("L") || eleccion.equals("C") || eleccion.equals("U"))
-                        && verificarElemento(eleccion, derecha))
+                if (verificarElemento(eleccion, derecha)
+                        && (eleccion.equals("L") || eleccion.equals("C") || eleccion.equals("U")))
                     moverElemento(eleccion, derecha, izquierda);
                 p_izquierda = true;
-                if ((verificarElemento("L", derecha) && verificarElemento("C", derecha))
-                        || verificarElemento("C", derecha) && verificarElemento("U", derecha)) {
-                    JOptionPane.showMessageDialog(null, "You lost :(");
-                    return;
-                }
             }
         } while (verificarElemento("L", izquierda) || verificarElemento("C", izquierda)
                 || verificarElemento("U", izquierda));
-        JOptionPane.showMessageDialog(null, "You win :)");
+        System.out.println("You win :)");
     }
 }
