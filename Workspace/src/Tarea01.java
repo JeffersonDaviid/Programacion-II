ef2b2e10367651735ee991372b882522f43d4edd
Jefferson <jeffersonchile2001@gmail.com>
2023-01-06 21:16:18 -0500

    login v2

--- Workspace/src/Tarea01.java
+++ Workspace/src/Tarea01.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Tarea01 {
     public static void presentarElementos(String objetos[], String etiqueta) {
         System.out.println(etiqueta);
@@ -16,15 +17,19 @@ public class Tarea01 {
 
     private static void moverElemento(String valor, String grupoSalida[], String grupoLlegada[]) {
         for (int elemento = 0; elemento < grupoSalida.length; elemento++)
-            if (verificarElemento(valor, grupoSalida) && grupoSalida[elemento].equals(valor) && !grupoSalida[elemento].equals("n"))
+            if (verificarElemento(valor, grupoSalida) && grupoSalida[elemento].equals(valor)
+                    && !grupoSalida[elemento].equals("n"))
                 grupoSalida[elemento] = "n";
         for (int elemento = 0; elemento < grupoLlegada.length; elemento++)
             if (grupoLlegada[elemento].equals("n") && (valor.equals("L") || valor.equals("C") || valor.equals("U"))) {
                 grupoLlegada[elemento] = valor;
                 break;
             }
-        if ((verificarElemento("L", grupoSalida) && verificarElemento("C", grupoSalida)) || verificarElemento("C", grupoSalida) && verificarElemento("U", grupoSalida)) {
-            System.out.println(verificarElemento("L", grupoSalida) && verificarElemento("C", grupoSalida) ? "\nPERDISTE :(, Lobo se comio a Caperucita" : "\nPERDISTE :(, Caperucita se comió las Uvas");
+        if ((verificarElemento("L", grupoSalida) && verificarElemento("C", grupoSalida))
+                || verificarElemento("C", grupoSalida) && verificarElemento("U", grupoSalida)) {
+            System.out.println(verificarElemento("L", grupoSalida) && verificarElemento("C", grupoSalida)
+                    ? "\nPERDISTE :(, Lobo se comio a Caperucita"
+                    : "\nPERDISTE :(, Caperucita se comió las Uvas");
             System.exit(0);
         }
     }
@@ -36,20 +41,24 @@ public class Tarea01 {
         String izquierda[] = { "L", "C", "U" };
         String derecha[] = { "n", "n", "n" };
         do {
-            presentarElementos(izquierda, "\nLos 3 elementos deben cruzar al otro lado del rio\nREGLAS DEL JUEGO:\n Si esta solo el lobo con la caperucita ( marcha la caperucita )\n Si la caperucita esta solo con las uvas ( marchan las uvas )\n\nElementos a la izquierda del rio");
+            presentarElementos(izquierda,
+                    "\nLos 3 elementos deben cruzar al otro lado del rio\nREGLAS DEL JUEGO:\n Si esta solo el lobo con la caperucita ( marcha la caperucita )\n Si la caperucita esta solo con las uvas ( marchan las uvas )\n\nElementos a la izquierda del rio");
             presentarElementos(derecha, "\nElementos a la derecha del rio");
             if (p_izquierda == true) {
-                System.out.println("\nEsta por dirigirse a la derecha, ¿Desea llevar un elemento? \n(Si) ingrese inicial del elemento\n(No) presione cualquier tecla ");
+                System.out.println(
+                        "\nEsta por dirigirse a la derecha, ¿Desea llevar un elemento? \n(Si) ingrese inicial del elemento\n(No) presione cualquier tecla ");
                 eleccion = teclado.next().toUpperCase();
                 moverElemento(eleccion, izquierda, derecha);
                 p_izquierda = false;
             } else {
-                System.out.println("\nEsta por volver a la izquierda, ¿Desea llevar un elemento? \n(Si) ingrese inicial del elemento\n(No) presione cualquier tecla ");
+                System.out.println(
+                        "\nEsta por volver a la izquierda, ¿Desea llevar un elemento? \n(Si) ingrese inicial del elemento\n(No) presione cualquier tecla ");
                 eleccion = teclado.next().toUpperCase();
                 moverElemento(eleccion, derecha, izquierda);
                 p_izquierda = true;
             }
-        } while (verificarElemento("L", izquierda) || verificarElemento("C", izquierda) || verificarElemento("U", izquierda));
+        } while (verificarElemento("L", izquierda) || verificarElemento("C", izquierda)
+                || verificarElemento("U", izquierda));
         System.out.println("FELICIDADES, todos los elemento lograron cruzar el río :)");
         teclado.close();
     }
